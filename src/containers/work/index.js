76a0e0e23112ef92a302dddc2478551d6b23ef6b
ayako <ayakosky@gmail.com>
2021-08-16 00:09:58 +0800

    [Features] Revamp work page

--- src/containers/work/index.js
+++ src/containers/work/index.js
@@ -3,16 +3,16 @@ import { routes } from 'router/routes';
 
 import './style.scss';
 
-const WorkPage = () => {
-  return (
-    <div className="">
+const WorkPage = () => (
+  <>
+    <div className="bb b--black-20">
       <Navbar currentPath="/work" />
-
-      <div>
-        <h1>Work page</h1>
-      </div>
     </div>
-  );
-};
+
+    <div className="tc mt5">
+      <h4>Work in progress...</h4>
+    </div>
+  </>
+);
 
 export default WorkPage;
